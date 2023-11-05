051775bf5c714a01497ea0ff76ec526828692ed8
Kartiktrk <86474555+Kartiktrk@users.noreply.github.com>
2023-11-05 19:55:48 +0530

    Add files via upload

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,13 @@ import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import projImg4 from "../assets/img/project-img4.png";
+import projImg5 from "../assets/img/project-img5.png";
+import projImg6 from "../assets/img/project-img6.png";
+import projImg7 from "../assets/img/project-img7.png";
+import projImg8 from "../assets/img/project-img8.png";
+import projImg9 from "../assets/img/project-img9.png";
+import projImg10 from "../assets/img/project-img10.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -11,33 +18,56 @@ export const Projects = () => {
 
   const projects = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Query It",
+      description: "DBMS",
+      imgUrl: projImg5,
+    },
+    {
+      title: "C Noobies",
+      description: "Data Structures and Algorithms",
       imgUrl: projImg1,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "OLPC",
+      description: "Data Structures and Algorithms",
       imgUrl: projImg2,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Reverse Coding",
+      description: "Data Structures and Algorithms",
       imgUrl: projImg3,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
+      title: "Code in Chaos",
+      description: "Data Structures and Algorithms",
+      imgUrl: projImg4,
+    },
+  ];
+
+  const projects1 = [
+    {
+      title: "Valorant",
+      description: "Gaming",
+      imgUrl: projImg8,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
+      title: "Shark Tank",
+      description: "Speaking and Selling",
+      imgUrl: projImg9,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Battle of Words",
+      description: "Speaking and Vocabulary",
+      imgUrl: projImg7,
+    },
+    {
+      title: "Mathomania",
+      description: "Mathematics and Problem Solving",
+      imgUrl: projImg6,
+    },
+    {
+      title: "Connextion",
+      description: "Linking and Problem Solving",
       imgUrl: projImg3,
     },
   ];
@@ -50,18 +80,18 @@ export const Projects = () => {
             <TrackVisibility>
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
+                <h2>Events</h2>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                      <Nav.Link eventKey="first">Technical</Nav.Link>
                     </Nav.Item>
+                    {/* <Nav.Item>
+                      <Nav.Link eventKey="second">Non-Technical</Nav.Link>
+                    </Nav.Item> */}
                     <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      <Nav.Link eventKey="third">Non Technical</Nav.Link>
                     </Nav.Item>
                   </Nav>
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
@@ -83,7 +113,18 @@ export const Projects = () => {
                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                      <Row>
+                        {
+                          projects1.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
                     </Tab.Pane>
                   </Tab.Content>
                 </Tab.Container>
